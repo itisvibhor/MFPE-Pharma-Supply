2af3d0b33f85d795640f251a2c23b58138f7da2b
itisvibhor <68295212+itisvibhor@users.noreply.github.com>
2022-07-12 16:55:28 +0530

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into pharmacies_table (pharmacy_name) values ('NewLife Pharmacy');
-insert into pharmacies_table (pharmacy_name) values ('MedCare Pharmacy');
-insert into pharmacies_table (pharmacy_name) values ('Bharat Pharmacy');
